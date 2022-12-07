c97ba5f5cff062ca4d1320453f46d1de1116fb3d
Muhammad Saad Siddique <26270646+MuhammaadSaad@users.noreply.github.com>
2022-12-07 21:17:23 +0500

    day 8 code

--- 2022/06/code.py
+++ 2022/06/code.py
@@ -1,17 +1,30 @@
 def code(data, chucksize):
     i=0
-    for s in range(chucksize, len(data)):
+    for s in range(2, len(data)):
         # check repeated char in chunk of 4
-        ch=  set(data[i:s])
-        i+=1
-        if len(ch) == chucksize:
+        
+        # 
+        
+        ch = set(data[i:s])
+        # print(i,s, data[i:s],ch)
+        i += 1
+        
+        # 
+        # 
+        if len(ch) == chucksize: #3==4
             return data[i:s], s
     return -1
+def sum(a,b):
+    print("sum")
+    print(a+b)
 
+#  print(i) in range(0,10))
 with open("input.txt") as file:
-    data=file.read()     
+    data=file.read()
+# data = "tnmmpfmfzmmnsmsjm"
 st,index= code(data,4)
-print(st,index)
+print(index)
 stP2,indexP2=code(data,14)
 print(stP2,indexP2)
+
 
